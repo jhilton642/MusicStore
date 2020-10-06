255b0e84375773e6229454ec57caf0595ab0cea5
jhilton642 <joshua.hilton@att.net>
2020-10-06 12:38:19 -0500

    Add project files.

---
+++ MusicStore/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WhiteRabbit()
+        {
+            return View();
+        }
+        public IActionResult ThisIsMe()
+        {
+            ViewBag.Now = DateTime.Now;
+            ViewBag.Name = "Josh Hilton";
+            return View();
+        }
+    }
+}
